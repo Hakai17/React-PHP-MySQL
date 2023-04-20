c709647353ebd38c903a1e107e2c1dc61c1f941f
Hakai <miguel.costa.arruda08@gmail.com>
2023-04-20 07:35:10 -0300

    created base api(in progress)

--- src/api/classes/product.php
+++ src/api/classes/product.php
@@ -27,35 +27,5 @@ class product extends connect{
         header("Content-type: application/json");
         echo json_encode($J);
     }
-
-    function addProduct(){
-        $body = json_decode(file_get_contents('php://input'), true);
-
-        $sql = "INSERT INTO products (sku, name, price, type, atributte) VALUES (";
-
-
-        $bodyValues = "'".implode("', '", array_values($body))."'";
-        $sql .= $bodyValues.");";
-
-        $db = DB::connect();
-        $query = $db->prepare($sql);
-        $queryResponse = $query->execute();
-    }
-
-    /* public function create()
-    {
-        $request = $_REQUEST;
-        $product = $this->model(ucfirst($request['productType']));
-        $product->add($request);
-        $this->redirect('/');
-    }
-
-    public function delete()
-    {
-        $request = $_REQUEST;
-        $product = new Product();
-        $product->delete($request);
-        $this->redirect('/');
-    } */
 }
 ?>
--- src/api/index.php
+++ src/api/index.php
@@ -2,15 +2,3 @@
 include("classes/product.php");
 $product=new product();
 $product->showProduct();
-
-if(isset($path[0])) { $action = $path[0]; } else { $action = ""; }
-
-$method = $_SERVER["REQUEST_METHOD"];
-
-if($method == "GET" && $action == "products") {
-    showProduct();
-}
-else if ($method == "POST" && $action == "products") {
-    addProduct();
-}
-?>
