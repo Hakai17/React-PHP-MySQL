f8d1536e53d1e181466dc91bdd48f3830f1bdf70
Hakai <miguel.costa.arruda08@gmail.com>
2023-04-20 07:22:44 -0300

    created home page(product list)

--- src/components/ListTest/ListTest.jsx
+++ src/components/ListTest/ListTest.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "./ListTest.css"
 
 function List() {
   const [list, setList] = useState([]);
@@ -18,25 +19,26 @@ function List() {
   return (
     <form id="product_form" action="/" method="post">
       <div className="row">
-        <div className="col-sm-4">
-          <div className="card product-card">
+        <div className="col-sm-12">
+          <div className="card">
             <div className="card-body">
-              <label>
-                <input
-                  type="checkbox"
-                  className="delete-checkbox"
-                  name=""
-                  value=""
-                />
-              </label>
               <div className="product-card-body">
-                <ul>
+                <ul className="product-card-ul">
                   {list.map((item) => (
                     <li key={item.id}>
+                      <label>
+                        <input
+                          type="checkbox"
+                          className="delete-checkbox"
+                          name={item.name}
+                          value={item.id}
+                        />
+                      </label>
                       <p className="product-card-text">{item.sku}</p>
                       <p className="product-card-text">{item.name}</p>
                       <p className="product-card-text">{item.price}</p>
                       <p className="product-card-text">{item.atributte}</p>
+                      <hr></hr>
                     </li>
                   ))}
                 </ul>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,32 @@
 import ListTest from "../../components/ListTest/ListTest";
+import Navbar from "../../components/Navbar/Navbar";
+import { useNavigate } from "react-router-dom";
+import "../../pages/pages.css";
+import Footer from "../../components/Footer/Footer";
 
 export default function Home() {
-    return(
-        <ListTest />
-    )
+  const navigate = useNavigate();
+  const redirectForAdd = () => {
+    navigate("/addproduct");
+  };
+
+  return (
+    <>
+      <div className="row">
+        <div className="col header-left">
+          <Navbar title="Product List" />
+        </div>
+        <div className="col header-right">
+          <div className="float-end">
+            <button onClick={redirectForAdd}>ADD</button>
+            <button id="delete-product-btn">MASS DELETE</button>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <ListTest />
+      <hr />
+      <Footer />
+    </>
+  );
 }
