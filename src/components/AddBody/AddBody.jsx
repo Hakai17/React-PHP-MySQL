b058a7b70e0e202f85d352327da5e7218f87ba7e
Hakai <miguel.costa.arruda08@gmail.com>
2023-04-20 07:23:08 -0300

    created first look of add product page

---
+++ src/components/AddBody/AddBody.jsx
@@ -0,0 +1,58 @@
+import Select from "../Select/Select"
+
+export default function AddBody() {
+  return (
+    <form id="product_form" action="/add-product" method="post">
+      <div className="col-sm-6">
+        <div className="mb-3 row">
+          <label htmlFor="sku" className="col-sm-2 col-form-label">
+            SKU
+          </label>
+          <div className="col-sm-6">
+            <input
+              type="text"
+              className="form-control"
+              name="sku"
+              id="sku"
+              required
+            />
+          </div>
+        </div>
+      </div>
+      <div className="col-sm-6">
+        <div className="mb-3 row">
+          <label htmlFor="name" className="col-sm-2 col-form-label">
+            Name
+          </label>
+          <div className="col-sm-6">
+            <input
+              type="text"
+              className="form-control"
+              name="name"
+              id="name"
+              required
+            />
+          </div>
+        </div>
+      </div>
+      <div className="col-sm-6">
+        <div className="mb-3 row">
+          <label htmlFor="price" className="col-sm-2 col-form-label">
+            Price ($)
+          </label>
+          <div className="col-sm-6">
+            <input
+              type="number"
+              className="form-control"
+              name="price"
+              id="price"
+              required
+            />
+          </div>
+        </div>
+      </div>
+      <Select />
+      <div className="col-sm-8" id="attributes"></div>
+    </form>
+  );
+}
--- src/pages/AddProduct/AddProduct.jsx
+++ src/pages/AddProduct/AddProduct.jsx
@@ -1,59 +1,27 @@
 import React from "react";
-import Select from "../../components/Select/Select";
+import Footer from "../../components/Footer/Footer";
+import AddBody from "../../components/AddBody/AddBody";
+import Navbar from "../../components/Navbar/Navbar";
+import "../../pages/pages.css"
 
 export default function AddProduct() {
   return (
-    <form id="product_form" action="/add-product" method="post">
-      <div className="col-sm-6">
-        <div className="mb-3 row">
-          <label htmlFor="sku" className="col-sm-2 col-form-label">
-            SKU
-          </label>
-          <div className="col-sm-6">
-            <input
-              type="text"
-              className="form-control"
-              name="sku"
-              id="sku"
-              required
-            />
+    <>
+      <div className="row">
+        <div className="col header-left">
+          <Navbar title="Product Add" />
+        </div>
+        <div className="col header-right">
+          <div className="float-end">
+            <button>Save</button>
+            <button>Cancel</button>
           </div>
         </div>
       </div>
-      <div className="col-sm-6">
-        <div className="mb-3 row">
-          <label htmlFor="name" className="col-sm-2 col-form-label">
-            Name
-          </label>
-          <div className="col-sm-6">
-            <input
-              type="text"
-              className="form-control"
-              name="name"
-              id="name"
-              required
-            />
-          </div>
-        </div>
-      </div>
-      <div className="col-sm-6">
-        <div className="mb-3 row">
-          <label htmlFor="price" className="col-sm-2 col-form-label">
-            Price ($)
-          </label>
-          <div className="col-sm-6">
-            <input
-              type="number"
-              className="form-control"
-              name="price"
-              id="price"
-              required
-            />
-          </div>
-        </div>
-      </div>
-      <Select />
-      <div className="col-sm-8" id="attributes"></div>
-    </form>
+      <hr />
+      <AddBody />
+      <hr />
+      <Footer />
+    </>
   );
 }
