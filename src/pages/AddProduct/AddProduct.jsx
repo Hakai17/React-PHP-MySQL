1e58a1b44a8d59a573782b41bbcd4a351b957489
Hakai17 <miguel.costa.arruda08@aluno.ifce.edu.br>
2023-04-29 15:58:01 -0300

    fix add page just to components in code

--- src/pages/AddProduct/AddProduct.jsx
+++ src/pages/AddProduct/AddProduct.jsx
@@ -1,24 +1,11 @@
 import React from "react";
 import Footer from "../../components/Footer/Footer";
 import AddBody from "../../components/AddBody/AddBody";
-import Navbar from "../../components/Navbar/Navbar";
-import "../../pages/pages.css"
+import "../../pages/pages.css";
 
 export default function AddProduct() {
   return (
     <>
-      <div className="row">
-        <div className="col header-left">
-          <Navbar title="Product Add" />
-        </div>
-        <div className="col header-right">
-          <div className="float-end">
-            <button>Save</button>
-            <button>Cancel</button>
-          </div>
-        </div>
-      </div>
-      <hr />
       <AddBody />
       <hr />
       <Footer />
